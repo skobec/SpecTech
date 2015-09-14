f33ae3ccac3a74a75e398cda0ca577bf4f999d40
Skobcow Roman <djromeo.91@mail.ru>
2015-09-14 20:04:35 +0300

    php2

--- send.php
+++ send.php
@@ -9,7 +9,7 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST[
 </head>
 <body>
 <p>Имя: '.$_POST['name'].'</p>
-<p>Телефон: '.$_POST['ph    one'].'</p>
+<p>Телефон: '.$_POST['phone'].'</p>
 </body>
 </html>'; //Текст нащего сообщения можно использовать HTML теги
     $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
