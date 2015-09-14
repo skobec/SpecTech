d7145e9dc3f710e678b1223545d9b41fb98e41dc
Skobcow Roman <djromeo.91@mail.ru>
2015-09-14 19:46:44 +0300

    php

--- send.php
+++ send.php
@@ -1,8 +1,8 @@
-<?
-        if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
-        $to = '275355@mail.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
-        $subject = 'Обратный звонок'; //Загаловок сообщения
-        $message = '
+<?php
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+    $to = '275355@mail.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+    $subject = 'Обратный звонок'; //Загаловок сообщения
+    $message = '
 <html>
 <head>
     <title>'.$subject.'</title>
@@ -12,8 +12,8 @@
 <p>Телефон: '.$_POST['phone'].'</p>
 </body>
 </html>'; //Текст нащего сообщения можно использовать HTML теги
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
-        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
-        }
-        ?>
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+    $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+    mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
